3b8a96e81fa7d8284080870bc612cc8040a0267a
tangerine-code <111338229+tangerine-code@users.noreply.github.com>
2022-11-01 14:56:00 +0200

    01.11.

--- ristinolla.py
+++ ristinolla.py
@@ -13,4 +13,24 @@ ikkuna.title('Ristinolla')
 
 ctk.CTkLabel(ikkuna, text = 'Ristinolla').pack()
 
+pelialue = ctk.CTkFrame(ikkuna, width = 300, height = 300)
+ruudukko = []
+class XOPoint:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.value = None
+        self.button = ctk.CTkButton(pelialue, text = '', width = 10, height = 5)
+        self.button.grid(row = x, column = y)
+
+    def CTkreset(self):  
+        self.button.configure(text="", bg='white')  
+        self.value = None  
+
+for x in range(1, 4):
+    for y in range(1, 4):
+        XOPoint(x, y)
+
+pelialue.pack(pady=10, padx=10)  
+
 ikkuna.mainloop()
--- testi.py
+++ testi.py
@@ -1,21 +1,19 @@
-import tkinter as tk
-import customtkinter
+import tkinter as tk  
   
 root = tk.Tk()  
 root.resizable(False, False)  
-root.title("Tic Tac Toe")
-root.configure(bg = 'black')
+root.title("Tic Tac Toe")  
   
 tk.Label(root, text="Tic Tac Toe", font=('Ariel', 25)).pack()  
   
-play_area = tk.Frame(root, width=300, height=300, bg='white')
+play_area = tk.Frame(root, width=300, height=300, bg='white')  
 XO_points = []  
 class XOPoint:  
     def __init__(self, x, y):  
         self.x = x  
         self.y = y  
         self.value = None  
-        self.button = tk.Button(play_area, text="", width=10, height=5, bg = 'red')  
+        self.button = tk.Button(play_area, text="", width=10, height=5)  
         self.button.grid(row=x, column=y)  
   
     def reset(self):  
