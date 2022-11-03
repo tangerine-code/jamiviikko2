6cce3fa85e7a7fd4d3e165734ede022e5fd7ad56
tangerine-code <111338229+tangerine-code@users.noreply.github.com>
2022-11-03 10:22:54 +0200

    Voiton tunnistus toimii

--- whatthefuck.py
+++ whatthefuck.py
@@ -31,8 +31,7 @@ class pelipainike(Button):
         self['image'] = self.kuva1
         
         self.bind('<Enter>', self.onEnter1)
-        self.bind('<Leave>', self.onLeave1)
-        
+        self.bind('<Leave>', self.onLeave1)        
 
     def onEnter1(self, event):
         if self.image(self.kuva1):
@@ -45,7 +44,6 @@ class pelipainike(Button):
             self.config(image=self.kuva1)
         else:
             self.config(image=self.kissa)
-
 
 def onEnter(event):
     """Kun kursori menee 'lopeta' napin päälle, kuva muuttuu tummemmaksi"""
@@ -100,8 +98,8 @@ iconi =PhotoImage(r'icon.ico')
 #________________________________PELIALUE_______________________________________
 
 vuoro = kissa
-kissa_pisteet = 0
-hiiri_pisteet = 0
+kissa_pisteet = []
+hiiri_pisteet = []
 play_area = tk.Frame(root, width = 300, height = 400)  
 #XO_points = []
 
@@ -123,16 +121,67 @@ class XOPoint:
             self.value = vuoro 
             if vuoro == kissa:
                 status_label.configure(text="Hiiren vuoro")
-                kissa_pisteet += 1
+                kissa_pisteet.append(self)
                 vuoro = hiiri
             elif vuoro == hiiri:
                 status_label.configure(text="Kissan vuoro")
-                hiiri_pisteet += 1
+                hiiri_pisteet.append(self)
                 vuoro = kissa
+        check_win()
 
 for x in range(1, 4):
     for y in range(1, 4):
         XOPoint(x, y)
+
+class Voittotilanne:
+    def __init__(self, x1, y1, x2, y2, x3, y3):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.x3 = x3
+        self.y3 = y3
+    def check(self, for_chr):
+        p1_satisfied = False
+        p2_satisfied = False
+        p3_satisfied = False
+
+        if for_chr == kissa:
+            for point in kissa_pisteet:
+                if point.x == self.x1 and point.y == self.y1:
+                    p1_satisfied = True
+                elif point.x == self.x2 and point.y == self.y2:
+                    p2_satisfied = True
+                elif point.x == self.x3 and point.y == self.y3:
+                    p3_satisfied = True
+        elif for_chr == hiiri:
+            for point in hiiri_pisteet:
+                if point.x == self.x1 and point.y == self.y1:
+                    p1_satisfied = True
+                elif point.x == self.x2 and point.y == self.y2:
+                    p2_satisfied = True
+                elif point.x == self.x3 and point.y == self.y3:
+                    p3_satisfied = True
+        return all([p1_satisfied, p2_satisfied, p3_satisfied])
+
+winning_possibilities = [
+    Voittotilanne(1, 1, 1, 2, 1, 3),
+    Voittotilanne(2, 1, 2, 2, 2, 3),
+    Voittotilanne(3, 1, 3, 2, 3, 3),
+    Voittotilanne(1, 1, 2, 1, 3, 1),
+    Voittotilanne(1, 2, 2, 2, 3, 2),
+    Voittotilanne(1, 3, 2, 3, 3, 3),
+    Voittotilanne(1, 1, 2, 2, 3, 3),
+    Voittotilanne(3, 1, 2, 2, 1, 3)
+]
+def check_win():
+    for possibility in winning_possibilities:
+        if possibility.check(kissa):
+            print("Kissa voitti!")
+            return
+        elif possibility.check(hiiri):
+            print("Hiiri voitti!")
+            return
 
 play_area.pack(pady = 10, padx = 10, side = tk.LEFT)
 #______________________________STATUS LABEL_____________________________________
@@ -140,6 +189,7 @@ play_area.pack(pady = 10, padx = 10, side = tk.LEFT)
 status_label = tk.Label(root, text=" ", font=('Verdana', '15', BOLD), 
 fg='#543f2a')
 status_label.place(x=410, y=130)
+
 #______________________________PELAAJIEN NIMET__________________________________
 
 lbl = Label(root, text="Pelaaja 1: Kissa", font=('Verdana', '15', BOLD), 
@@ -152,18 +202,17 @@ lbl2.place(x=410, y=60)
 
 #____________________________UUDESTAAN JA LOPETA________________________________
 
-lopeta_nappi = Button(root, image = lopeta, borderwidth=0, height= 75, width=189,
-command=root.destroy)
+lopeta_nappi = Button(root, image = lopeta, borderwidth=0, height= 75, width=189, command=root.destroy)
 lopeta_nappi.place(x=400, y=285)
-lopeta_nappi.bind('<Enter>',  onEnter)
-lopeta_nappi.bind('<Leave>',  onLeave)
+lopeta_nappi.bind('<Enter>', onEnter)
+lopeta_nappi.bind('<Leave>', onLeave)
 
-uudestaan_nappi = Button(root, image = uudestaan, borderwidth=0, height= 75, width=189,
-command = uusiksi)
+uudestaan_nappi = Button(root, image = uudestaan, borderwidth=0, height= 75, width=189, command = uusiksi)
 uudestaan_nappi.place(x=400, y=200)
-uudestaan_nappi.bind('<Enter>',  onEnter2)
-uudestaan_nappi.bind('<Leave>',  onLeave2)
+uudestaan_nappi.bind('<Enter>', onEnter2)
+uudestaan_nappi.bind('<Leave>', onLeave2)
 #___________________________________MUUT________________________________________
+
 status_label.configure(text="Kissan vuoro")
 windll.shcore.SetProcessDpiAwareness(1)
 root.iconbitmap(iconi)
